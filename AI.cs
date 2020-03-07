e9ac37650fa87a004a36bb424487fc91b84bd313
Chicais <60269046+Chicais@users.noreply.github.com>
2020-03-07 23:36:17 +0100

    Add files via upload
    code pour les objets  qui augmente la vitesse ou le saut + tentative IA

---
+++ AI.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    /*public float moveSpeed;
+    public float jumpForce;
+    public float gravityScale;
+    public GameObject Finish;
+    public GameObject Character;
+    public bool isSpeedBoosted = false;
+    public bool isJumpBoosted = false;
+    public float currentTime = 0f;
+    public float startingTime = 10f;*/
+    public Transform target;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+        /*if (isSpeedBoosted)
+        {
+            moveSpeed = 30f;
+            currentTime -= 1 * Time.deltaTime;
+            if (currentTime <= 0)
+            {
+                moveSpeed = 10f;
+                isSpeedBoosted = false;
+            }
+        }
+        
+        if (isJumpBoosted)
+        {
+            jumpForce = 12f;
+            currentTime -= 1 * Time.deltaTime;
+            if (currentTime <= 0)
+            {
+                jumpForce = 8f;
+                isJumpBoosted = false;
+            }
+        }
+        
+        Character.transform.position = Finish.transform.position;*/
+    }
+    
+    /*public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Checkpoint"))
+        {
+            currentTime = startingTime;
+            isSpeedBoosted = true;
+        }
+        
+        if (other.gameObject.CompareTag("Jump Boost"))
+        {
+            currentTime = startingTime;
+            isJumpBoosted = true;
+        }
+        
+        
+    }*/
+}
---
+++ Checkpoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public Renderer theRend;
+    public Material cpOff;
+    public Material cpOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /*public void CheckpointOn()
+    {
+        theRend.material = cpOn;
+    }*/
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name=="Player")
+        {
+            //CheckpointOn();
+            Destroy(gameObject);
+        }
+    }
+}
